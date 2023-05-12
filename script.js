56602dfb53f1899606e0cc814c2c843ef15166d2
Niranjan <niranjan2257@gmail.com>
2023-05-12 08:17:45 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,17 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "150px";
+    document.getElementById("main").style.marginLeft = "150px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+  function openContact() {
+    document.getElementById("contact_window").style.width = "100%";
+  }
+  
+  function closeContact() {
+    document.getElementById("contact_window").style.width = "0";
+  }
